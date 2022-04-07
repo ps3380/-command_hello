b5f2a2558e6ddc25945f161cfd2dea4324bb4eb9
rhino <rhino@deagu.ac.kr>
2022-04-07 11:10:21 +0900

    re

---
+++ hello world1/src/hello.java
@@ -0,0 +1,8 @@
+
+public class hello {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Hello his World");
+		System.out.println("Hello her World");
+	}
+}
